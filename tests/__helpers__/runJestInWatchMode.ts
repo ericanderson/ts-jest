8550244b4241192382a7929176de7ba803634039
Igor Chulinda <ichulinda@dataart.com>
2016-11-07 12:15:32 +0100

    Output for end if statement.

--- tests/__helpers__/runJestInWatchMode.ts
+++ tests/__helpers__/runJestInWatchMode.ts
@@ -35,15 +35,18 @@ export default function runJestInWatchMode(dir, args?: any[]) {
   const childProcess = spawn(JEST_PATH, args, {
     cwd: dir,
   });
+  let promiseNumber = 0;
   let getStderrAsync = () => {
     return new Promise((resolve: (value: string) => void) => {
       let stderr = '';
+      let index = promiseNumber + 1;
+      promiseNumber++;
       let listener = (data) => {
         stderr += data.toString();
-        console.log(data.toString());
+        console.log('#' + index.toString() + ' ' + data.toString().includes('Ran all').toString());
         if (data.toString().includes('Ran all')) {
           resolve(stderr);
-          // childProcess.stderr.removeListener('data', listener);
+          childProcess.stderr.removeListener('data', listener);
         }
       };
       childProcess.stderr.on('data', listener);
